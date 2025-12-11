8fdb87ea905e6cff3318a6143e24c4962b6628d5
PRO <pro@MacBook-Pro-de-PRO.local>
2025-12-11 15:38:41 +0100

    add new filtsers

--- discount/models.py
+++ discount/models.py
@@ -11,7 +11,7 @@ class CODProduct(models.Model):
     name = models.CharField(max_length=600)
     original_price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
-    image_url = models.URLField(blank=True, null=True)
+    image_url = models.TextField(blank=True, null=True , )
     is_active = models.BooleanField(default=True)  # هل المنتج متاح للعرض؟
     last_updated = models.DateTimeField(auto_now=True)  # تاريخ آخر تحديث
     product_cost= models.DecimalField(max_digits=10 ,decimal_places=2,blank=True, null=True)
@@ -21,12 +21,6 @@ class CODProduct(models.Model):
     productImage = models.ImageField(upload_to='products/', null=True, blank=True)
 
 
-
-    # custom_name = models.CharField(max_length=255, blank=True, null=True)
-    # custom_image = models.URLField(blank=True, null=True)
-
-    # created_at = models.DateTimeField(auto_now_add=True ,blank=True, null=True)
-    # updated_at = models.DateTimeField(auto_now=True ,blank=True, null=True)
     def __str__(self):
         return self.name
 
